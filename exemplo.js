566431bd5ae7e563df28462ac6a380698a9a9117
Igor Xavier <igor.xavier.simoes@gmail.com>
2021-06-30 07:37:34 -0300

    front

--- exemplo.js
+++ exemplo.js
@@ -1,11 +1,11 @@
-const readline = require('readline');
+const readline = require(`readline`);
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question('QUAL SEU NOME:  ', (answer) => {
+rl.question(`QUAL SEU NOME:  `, (answer) => {
     // TODO: Log the answer in a database
     console.log(`BEM VINDO: ${answer}`);
 
